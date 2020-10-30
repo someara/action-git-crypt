d7f8542f98776f0a0c251b5e1250c9e5b115ce78
Sean OMeara <sean@sean.io>
2020-10-30 02:15:27 +0100

    tweaking

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,18 @@ echo ${GPG_KEY_PASS?"please set GPG_KEY_PASS"} > /dev/null
 echo "$GPG_PRIVATE_KEY" | base64 -d > "$HOME"/git-crypt-key.asc
 gpg --batch --import "$HOME"/git-crypt-key.asc
 
-echo "gpg -K"
-gpg -K
+echo "gpg --with-keygrip --list-secret-keys"
+gpg --with-keygrip --list-secret-keys
 
 echo "GPG_KEY_GRIP: $GPG_KEY_GRIP"
 
 gpgconf --kill gpg-agent
 gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
+
+echo"/usr/lib/gnupg2/gpg-preset-passphrase --preset --passphrase \$GPG_KEY_PASS \$GPG_KEY_GRIP"
 /usr/lib/gnupg2/gpg-preset-passphrase --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
 
+echo "git-crypt unlock"
 git-crypt unlock
 
 rm "$HOME"/git-crypt-key.asc
